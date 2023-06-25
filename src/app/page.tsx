66387472e3135476e82a1a9c79f29756c1b78671
Jian yong Lei <jianyonglei@gmail.com>
2023-06-24 21:15:11 -0400

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Icons } from '@/components/Icons'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* Header */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Projects */}
+
+      {/* Skills */}
+
+      {/* Contact */}
+    </main>
+  )
+}
---
+++ src/components/Icons.tsx
@@ -0,0 +1,26 @@
+import { LucideProps } from 'lucide-react'
+
+export const Icons = {
+  logo: (props: LucideProps) => (
+    <svg
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        {' '}
+        <path
+          d="M15.5 15L18.5 12L15.5 9M8.5 9L5.5 12L8.5 15M13 7L11 17M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+          stroke="#000000"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"></path>{' '}
+      </g>
+    </svg>
+  )
+}
